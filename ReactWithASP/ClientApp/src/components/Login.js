8204ddea3724b372445bee1b31537a40c5640a5b
alex323237777 <Alex.Jack@student.shu.ac.uk>
2025-02-18 16:46:58 +0000

    I hate coding

---
+++ ReactWithASP/ClientApp/src/components/Login.js
@@ -0,0 +1,55 @@
+﻿import React from 'react';
+import './Login.css';
+import companyLogo from '../images/daisy-logo.jpg'; // Adjust the path as necessary
+import backgroundImg from '../images/LoginPageLeft.jpg'; // Adjust the path as necessary
+
+const Login = () => {
+    return (
+        <div className="container">
+            <div className="top-side">
+                <img src={backgroundImg} alt="Background" className="background-img" />
+            </div>
+
+
+            {/* Right Side (Login Form and Logo) */}
+            <div className="right-side">
+                <div className="login-box">
+                    {/* Logo at the top of the right side */}
+                    <div className="logo">
+                        <img src={companyLogo} alt="Company Logo" />
+                    </div>
+                    {/* Header: My Account - Login */}
+                    <div className="header">My Account – Login</div>
+
+                    <form>
+                        {/* Username Input */}
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" id="username" name="username" className="input-field" required />
+                        </div>
+
+                        {/* Password Input */}
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" name="password" className="input-field" required />
+                        </div>
+
+                        {/* Links for Forgot Password and Create Account */}
+                        <div className="links">
+                            <a href="/forgot-password">Forgot password?</a>
+                            <a href="#">Create Account</a>
+                        </div>
+
+                        {/* Submit Button */}
+                        <button type="submit" className="submit-btn">Sign In</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
+
+
+
---
+++ ReactWithASP/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ReactWithASP.Models
+{
+    public class LoginModel
+    {
+        public string agentUsername { get; set; }
+        public string agentPassword { get; set; }
+    }
+}
