ce18ebe3ffd514a12086303026e3be64f046913d
Carter Tro <c3025886@hallam.shu.ac.uk>
2025-02-19 13:18:41 +0000

    mannnnnn

---
+++ ReactWithASP/Models/Agent.cs
@@ -0,0 +1,11 @@
+namespace ReactWithASP.Models {
+
+    public class Agent {
+
+        public int agentId { get; set; }
+        public string agentName { get; set; }
+        public string agentUsername { get; set; }
+        public string agentPassword { get; set; }
+
+    }
+}
